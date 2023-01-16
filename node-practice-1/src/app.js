8f431e32109fc66803e78e8ac17e8957cdc943ef
He Xiang Koh <kohhxx6@gmail.com>
2023-01-16 10:16:03 +0800

    1 - routes that send HTML or JSON/Object

--- node-practice-1/src/app.js
+++ node-practice-1/src/app.js
@@ -2,32 +2,38 @@ const express = require("express");
 
 const app = express();
 
-
-
-
-
-
 // Create routes
-app.get('/', (req,res) => {
-  res.send("Hello express")
-})
+app.get("/", (req, res) => {
+  res.send("Hello express");
+});
 
-app.get('/help', (req,res) => {
-  res.send("help pages")
-})
+app.get("/help", (req, res) => {
+  res.send([
+    {
+      name: "KOHHX",
+      age: 100,
+    },
+    {
+      name: "KY",
+      age: 200,
+    },
+  ]);
+});
 
-app.get('/about', (req,res) => {
-  res.send("about")
-})
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>"); // Send a HTML string
+});
 
-app.get('/weather', (req,res) => {
-  res.send("weather")
-})
-
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is sunny",
+    location: "Singapore",
+  });
+});
 
 // Listen to port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server Running on PORT " + PORT + "...")
-})
+  console.log("Server Running on PORT " + PORT + "...");
+});
