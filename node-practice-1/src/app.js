b915fe8c43cc5f58d1dd759fa0937d6dce58e4eb
He Xiang Koh <kohhxx6@gmail.com>
2023-01-16 10:56:03 +0800

    added handlebar view engine and serve webpages

--- node-practice-1/src/app.js
+++ node-practice-1/src/app.js
@@ -7,33 +7,41 @@ const express = require("express");
 // Initlaize App
 const app = express();
 
+// Set View Engine
+app.set("view engine", "hbs");
+
 //Servce public folder (index)
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
 // Create routes
 
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "KOHHX",
-      age: 100,
-    },
-    {
-      name: "KY",
-      age: 200,
-    },
-  ]);
-});
-
-
-
-app.get("/weather", (req, res) => {
-  res.send({
-    forecast: "It is sunny",
-    location: "Singapore",
+app.get("/", (req, res) => {
+  res.render("index", {
+     title: "Weather",
+     name: "Kohhx",
   });
 });
+
+app.get("/about", (req, res) => {
+  res.render('about', {
+    title: "About me",
+    name: "Kohhx"
+  })
+})
+
+app.get("/help", (req, res) => {
+  res.render('help', {
+    message: "Help....",
+  })
+})
+
+// app.get("/weather", (req, res) => {
+//   res.send({
+//     forecast: "It is sunny",
+//     location: "Singapore",
+//   });
+// });
 
 // Listen to port
 const PORT = process.env.PORT || 5000;
