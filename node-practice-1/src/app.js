cecc9d76a8e40384bcb5a2046bfa6c3e3bdafa2c
He Xiang Koh <kohhxx6@gmail.com>
2023-01-16 11:18:32 +0800

    configure template folder in put views and partials folder in. Require hbs and configure partial to hbs

--- node-practice-1/src/app.js
+++ node-practice-1/src/app.js
@@ -3,45 +3,55 @@ const path = require("path");
 
 //Require NPM Modules
 const express = require("express");
+const  hbs  = require('hbs');
 
 // Initlaize App
 const app = express();
 
-// Set View Engine
+// Set View Engine (Handlebars) ================================================
 app.set("view engine", "hbs");
+// Serve View Folder ( this has to be set cause we move view folder into
+// template folder)
+const viewsPath = path.join(__dirname, "../templates/views");
+app.set("views", viewsPath)
+// Serve partials
+const partialsPath = path.join(__dirname,"../templates/partials")
+hbs.registerPartials(partialsPath)
 
-//Servce public folder (index)
+
+
+//Serve static directory - public folder =======================================
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
-// Create routes
 
+// Create routes ===============================================================
 app.get("/", (req, res) => {
   res.render("index", {
-     title: "Weather",
-     name: "Kohhx",
+    title: "Weather",
+    name: "Kohhx",
   });
 });
 
 app.get("/about", (req, res) => {
-  res.render('about', {
+  res.render("about", {
     title: "About me",
-    name: "Kohhx"
-  })
-})
+    name: "Kohhx",
+  });
+});
 
 app.get("/help", (req, res) => {
-  res.render('help', {
+  res.render("help", {
     message: "Help....",
-  })
-})
+  });
+});
 
-// app.get("/weather", (req, res) => {
-//   res.send({
-//     forecast: "It is sunny",
-//     location: "Singapore",
-//   });
-// });
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is sunny",
+    location: "Singapore",
+  });
+});
 
 // Listen to port
 const PORT = process.env.PORT || 5000;
